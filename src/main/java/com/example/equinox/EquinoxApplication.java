c77d3fae9f55a15663264628ef6908d5fd0070a3
gokhangultan <gultangokhan@gmail.com>
2024-06-11 13:17:39 +0300

    first commit pomxl

---
+++ src/main/java/com/example/equinox/EquinoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.equinox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquinoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquinoxApplication.class, args);
+	}
+
+}
