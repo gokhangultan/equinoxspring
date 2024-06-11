5fe102da9821b9bd1540da02b09975c1100e9072
gokhangultan <gultangokhan@gmail.com>
2024-06-11 13:31:17 +0300

    security config with cors

---
+++ src/main/java/com/example/equinox/config/SecurityConfig.java
@@ -0,0 +1,67 @@
+package com.example.equinox.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public AuthenticationManager authManager(UserDetailsService userDetailsService){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173","https://equinox.gokhangultan.com"));
+        corsConfiguration.setAllowedMethods(List.of("GET","PUT","DELETE","POST"));
+        corsConfiguration.setAllowedHeaders(List.of(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return source;
+
+
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.cors().configurationSource(corsConfigurationSource());
+        return httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth->{
+                    auth.requestMatchers("/products/**").permitAll();
+                    auth.requestMatchers("/auth/**").permitAll();
+                    //auth.requestMatchers("/categories/**").hasAnyAuthority("USER", "ADMIN");
+                    auth.requestMatchers("/admin/**").hasAuthority("ADMIN");
+                    auth.anyRequest().authenticated();
+                })
+                //.formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
